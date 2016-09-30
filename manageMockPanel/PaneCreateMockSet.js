96fca69bf8b94228ca1477fb263832309396d364
Benjamin Egner <benjamin.egner@aoe.com>
2016-09-30 15:12:22 +0200

    Quicksearch

--- manageMockPanel/PaneCreateMockSet.js
+++ manageMockPanel/PaneCreateMockSet.js
@@ -6,7 +6,51 @@ var PaneCreateMockSet = function(navigation) {
     this.apiBridge = new ApiBridge();
     this.bindEvents();
 
-	this.$multiSelect.multiSelect();
+	// Make multi select box fancy
+	// http://loudev.com/
+	this.$multiSelect.multiSelect({
+		selectableHeader: "<input type='text' class='search-input' autocomplete='off' placeholder=''>",
+		selectionHeader: "<input type='text' class='search-input' autocomplete='off' placeholder=''>",
+		afterInit: function(ms) {
+			var that = this;
+			var $selectableSearch = that.$selectableUl.prev();
+			var $selectionSearch = that.$selectionUl.prev();
+			var selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)';
+			var selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
+
+			that.qs1 = $selectableSearch.quicksearch(selectableSearchString).on('keydown', function(e){
+
+				if (e.which === 13){
+					e.preventDefault();
+				}
+
+				if (e.which === 40){
+					that.$selectableUl.focus();
+					return false;
+				}
+			});
+
+			that.qs2 = $selectionSearch.quicksearch(selectionSearchString).on('keydown', function(e) {
+
+				if (e.which === 13){
+					e.preventDefault();
+				}
+
+				if (e.which == 40){
+					that.$selectionUl.focus();
+					return false;
+				}
+			});
+		},
+		afterSelect: function(){
+			this.qs1.cache();
+			this.qs2.cache();
+		},
+		afterDeselect: function(){
+			this.qs1.cache();
+			this.qs2.cache();
+		}
+  });
 };
 
 // Draw pane
@@ -16,8 +60,7 @@ PaneCreateMockSet.prototype.draw = function() {
 	this.$form.trigger('reset');    
 	$('#set_form_id').val('');
 
-    // Make multi select box fancy
-    // http://loudev.com/
+
 
     this.$multiSelect.multiSelect('deselect_all');
 
