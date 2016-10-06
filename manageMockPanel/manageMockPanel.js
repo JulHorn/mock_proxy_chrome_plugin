586a31f038202f2543de359d934a5e50275bd91b
julian.horn <julian.horn@aoe.com>
2016-10-06 15:35:17 +0200

    Enables the user to have more than one endpoint configured in the options and to be able to activate the one he needs.

--- manageMockPanel/manageMockPanel.js
+++ manageMockPanel/manageMockPanel.js
@@ -2,11 +2,7 @@
 
 // If the local storage value does not exist (user never changed anything ot it is empty), then warn the user
 // and use a default value
-if (!localStorage.mockProxyServerTargetEndpoint) {
-	localStorage.mockProxyServerTargetEndpoint = "http://localhost:8001";
-	alert("No target endpoint configuration found. Requests target is: " + localStorage.mockProxyServerTargetEndpoint);
-}
-
+new LocalStorageHandler().setDefaultEndpointIfNoEndpointIsSet();
 
 $(document).ready(function() {
 
@@ -22,7 +18,7 @@ $(document).ready(function() {
 		styleSheetName = 'page';
 	}
 
-	$('head').append('<link rel="stylesheet" href="../css/'+styleSheetName+'.css">');
+	$('head').append('<link rel="stylesheet" href="../css/' + styleSheetName + '.css">');
 	$('body').addClass(styleSheetName);
 
 	new UiNavigation();
